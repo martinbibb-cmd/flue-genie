2c2cc6423bb2dd85a5a4f333604e131fa0ce0ad2
martinbibb-cmd <martinbibb@gmail.com>
2025-11-10 15:28:45 +0000

    Create flue clearance sandbox app

---
+++ data/manufacturerRules.js
@@ -0,0 +1,60 @@
+const MANUFACTURER_RULES = {
+  worcester: {
+    label: "Worcester",
+    clearances: {
+      toOpening: 300,
+      belowEaves: 200,
+      belowGutter: 75,
+      reducedWithExtension: 25,
+      toFacingSurface: 600,
+      toBoundary: 300,
+      terminalFacingTerminal: 1200,
+      verticalOnSameWall: 1500,
+      toOppositeOpening: 2000
+    }
+  },
+  vaillant: {
+    label: "Vaillant / ecoTEC",
+    clearances: {
+      toOpening: 300,
+      belowEaves: 200,
+      belowGutter: 75,
+      reducedWithExtension: 25,
+      toFacingSurface: 600,
+      toBoundary: 300,
+      terminalFacingTerminal: 1200,
+      verticalOnSameWall: 1500,
+      toOppositeOpening: 2000
+    }
+  },
+  ideal: {
+    label: "Ideal Logic+",
+    clearances: {
+      toOpening: 300,
+      belowEaves: 200,
+      belowGutter: 75,
+      reducedWithExtension: 25,
+      toFacingSurface: 600,
+      toBoundary: 300,
+      terminalFacingTerminal: 1200,
+      verticalOnSameWall: 1500,
+      toOppositeOpening: 2000
+    }
+  },
+  viessmann: {
+    label: "Viessmann (BS 5440 set)",
+    clearances: {
+      toOpening: 300,
+      belowEaves: 200,
+      belowGutter: 75,
+      reducedWithExtension: 25,
+      toFacingSurface: 600,
+      toBoundary: 300,
+      terminalFacingTerminal: 1200,
+      verticalOnSameWall: 1500,
+      toOppositeOpening: 2000
+    }
+  }
+};
+
+export { MANUFACTURER_RULES };
